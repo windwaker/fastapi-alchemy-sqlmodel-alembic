75c9566c3a02e4713bf0472c687f4f157bc2dc7f
colm <colm.harrington@gmail.com>
2021-10-11 15:02:43 +0100

    added tasks to ReadMe

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Tasks:
 
 1. Rework song endpoints to abstract out crud functions
 2. Inject special test DB with seeded data for tests
+   1. https://fastapi.tiangolo.com/advanced/testing-database/
 3. Inject real app for integration version of tests to ensure we don't maintain two suites of tests
 4. Rework Song endpoints to work for players
-5. 
+5. Split APIs into separate routes & files
--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from sqlmodel import SQLModel, Field
 
 from typing import Optional
@@ -15,3 +17,13 @@ class Song(SongBase, table=True):
 
 class SongCreate(SongBase):
     pass
+
+
+class PlayerBase(SQLModel):
+    name: str  # = Field(min_length=2, max_length=50, )
+    # https://pydantic-docs.helpmanual.io/usage/types/#datetime-types
+    date_of_birth: str  # datetime = Field(default=None)
+
+
+class Player(PlayerBase, table=True):
+    id: int = Field(default=None, primary_key=True)
